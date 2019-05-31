5517d3cb8662cc224db3af18f4c9b3d105efd958
SETHISA <kasunrajitha733@gmail.com>
2019-05-31 16:36:15 +0530

    dd

--- employee-service/src/main/java/com/example/demo/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/example/demo/EmployeeServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.oauth2.client.test.OAuth2ContextConfiguration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
